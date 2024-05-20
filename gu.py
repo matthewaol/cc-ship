07f7bbdac93140b9ad9f8cde62218b8d8d1ce931
matthewaol <matthewdacanay@aol.com>
2024-05-19 17:05:49 -0700

    cool gui

--- gu.py
+++ gu.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import scrolledtext 
+from tkinter import messagebox
 import pandas as pd
 
 ships_df = pd.read_csv("stem_center_internships.csv")
@@ -9,17 +11,31 @@ def find_major():
     result = ""
     if "computer" in major.lower():
         result = ships_df[ships_df['Discipline'].str.contains('Computer|Coding|Multiple', case=False, na=False)]
+
     elif "engineering" in major.lower():
         result = ships_df[ships_df['Discipline'].str.contains('Engineering|Multiple', case=False, na=False)]
     
+    names = result['Internship Opportunity']
+    links = result['Application Link']
+    names_links = dict(zip(names,links))    
+
     if result is not None:
-        messagebox.showinfo("Here's your internships", result.to_string(index=False))
+        display_result(names_links)
     else: 
         messagebox.showinfo("Here's your internships", "? Change majors")
 
+def display_result(result_dict):
+    result_window = tk.Toplevel(root)
+
+    text_widget = scrolledtext.ScrolledText(result_window, wrap=tk.WORD)
+    text_widget.pack(expand=True, fill="both")
+
+    for key, value in result_dict.items():
+        text_widget.insert(tk.END,'\n')
+        text_widget.insert(tk.END, f"{key}: {value}\n")
 
 # Create GUI
-root = tk.Tk()
+root = tk.Tk()  
 root.title("Internship Finder")
 
 label = tk.Label(root, text="Enter major:")
